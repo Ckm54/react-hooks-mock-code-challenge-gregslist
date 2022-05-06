600741f278531098d4885170ecceba8d06386147
Collins <mutaic84@gmail.com>
2022-05-06 16:27:41 +0300

    handle requests in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:6001/listings`)
+    .then((res) => res.json())
+    .then((fetchedListings) => setListings(fetchedListings))
+  }, [])
+
+  function onItemDelete(item) {
+    fetch(`http://localhost:6001/listings/${item.id}`, {
+      method: "DELETE"
+    })
+    .then((res) => res.json())
+    .then(() => {
+      const remainingListings = listings.filter((listing) => listing.id !== item.id)
+      setListings(remainingListings)
+    })
+  }
+
+  function handleUserSearch(nameInput) {
+    const searchResult = listings.filter((listing) => listing.description === nameInput)
+    setListings(searchResult)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearchHandle={ handleUserSearch }/>
+      <ListingsContainer productListings={listings} handleListingDelete={onItemDelete}/>
     </div>
   );
 }
