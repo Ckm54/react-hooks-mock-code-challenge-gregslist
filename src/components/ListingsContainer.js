45443dbd5194ef078fcc8762037a96a61bf4e16d
Collins <mutaic84@gmail.com>
2022-05-06 16:38:21 +0300

    create button to handle sorting listings

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ListingCard from "./ListingCard";
 
-function ListingsContainer({ productListings, handleListingDelete }) {
+function ListingsContainer({ productListings, handleListingDelete, handleListingSort }) {
 
   const myListings = productListings.map((listing) => <ListingCard key={listing.id} item={listing} onDeleteItem={handleListingDelete}/>)
   return (
     <main>
+      <button onClick={handleListingSort}>Sort listings by locations [A - Z]</button>
       <ul className="cards">
         { myListings }
       </ul>
